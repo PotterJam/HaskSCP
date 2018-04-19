811eee99d443a7c9cc2b9e39910aa8426b2a68ba
James Potter <sanotter@gmail.com>
2018-04-19 02:11:08 +0100

    Created basic searching for files in remote system

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
 module Main where
 
 import Lib
+import Data.Text.Lazy.Encoding
+import qualified Data.Text.Lazy.IO as TIO
+import Network.SSH.Client.LibSSH2
 
 main :: IO ()
-main = someFunc
+main = do
+  size <- testSSH2User testScp
+  putStr $ show size
+
+testSSH2User :: (Session -> IO a) -> IO a
+testSSH2User = withSSH2User "/home/james/Scripts" "" "" "" 22
+
+testScp :: Session -> IO Integer
+testScp = scpReceive "" ""
+
+scpReceive :: FilePath -> FilePath -> Session -> IO Integer
+scpReceive src dest s = do
+  requestPath <- findFileToRequest src s
+  scpReceiveFile s requestPath dest
+
+findFileToRequest :: FilePath -> Session -> IO String
+findFileToRequest src s = do
+  putStrLn $ "Looking in " ++ src ++ ":"
+  let lsCmd = "ls " ++ src
+  (_, (x:_)) <- execCommands s [lsCmd]
+  TIO.putStrLn $ decodeUtf8 x
+  putStrLn "grep: "
+  toGrep <- getLine
+  let grepCmd = "grep -i " ++ toGrep
+  (_, (y:_)) <- execCommands s [lsCmd ++ " | " ++ grepCmd]
+  TIO.putStrLn $ decodeUtf8 y
+  putStrLn "Please enter what you would like to download."
+  fileName <- getLine
+  return $ src ++ "/" ++ fileName
+
